e13bc6287f0666cdb0507ddef55d287f2466976f
Bharat Choudhary <Choudhary_b@India.XoriantCorp.com>
2022-08-29 11:18:48 +0530

    Updated Version 1

--- videotester.py
+++ videotester.py
@@ -14,7 +14,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # load model
-model = load_model("best_model2.h5")
+model = load_model("model1.h5")
 
 face_haar_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
@@ -30,7 +30,7 @@ while True:
     faces_detected = face_haar_cascade.detectMultiScale(gray_img, 1.32, 5)
 
     for (x, y, w, h) in faces_detected:
-        cv2.rectangle(test_img, (x, y), (x + w, y + h), (255, 0, 0), thickness=7)
+        cv2.rectangle(test_img, (x, y), (x + w, y + h), (255, 0, 0), thickness=4)
         roi_gray = gray_img[y:y + w, x:x + h]  # cropping region of interest i.e. face area from  image
         roi_gray = cv2.resize(roi_gray, (224, 224))
         img_pixels = image.img_to_array(roi_gray)
